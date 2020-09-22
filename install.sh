3e7543366c3b776f79388ca8561f61dfcf411214
ChipCE <login721@gmail.com>
2020-09-22 16:38:19 +0900

    update build script

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo "$CURRENT_DIR"
+# echo "$CURRENT_DIR"
+
+if [ "$1" = "--build" ]
+then
+    cd "$CURRENT_DIR/lib"
+    make clean
+    make all
+    make install
+fi
+
 cd ~/.node-red
 npm install "$CURRENT_DIR"
